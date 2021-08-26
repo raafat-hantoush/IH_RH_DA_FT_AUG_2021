be33c211532d6228deba5c316a38ae2de7d7ad6e
Rafa <raafat.hantoush@gmail.com>
2021-08-26 16:29:43 +0200

    week 3

---
+++ Class Materials/SQL_MySQL/Labs/Lab_SQL_Stored_Procedures.sql
@@ -0,0 +1,13 @@
+use olist;
+
+/*
+
+Your manager now want to have these KPIs as SQL stored procedures, 
+so that they can run them anytime from the server.
+You should write procedures that output the following:
+
+1.  A list of cities and their revenue, sorted by revenue, only showing cities that belong to states with a revenue above a certain threshold.
+1.2 BONUS: The previous procedure only outputs the top 3 cities by each state.
+2. Top N cities by Avg Order amount.
+
+*/
